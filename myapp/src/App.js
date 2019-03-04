1bd57e89054d74fbd3e88014081da7814be0020b
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-03-04 00:10:30 -0500

    initial commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,12 +3,20 @@ import Ninjas from './ninjas';
 
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 20, belt: 'black', id: 1 }, 
+      { name: 'Yoshi', age: 1, belt: 'black', id: 2 },
+      { name: 'Buddy', age: 9, belt: 'golden', id: 3 }
+    ]
+  }
   render() {
+    
     return (
       <div className="App">
         <h1>My First React app</h1>
         <p>Welcome :)</p>
-        <Ninjas />
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
