a55c4a8c277f12a9b59bc4ac4e03973795bf46c4
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-03-10 00:19:23 -0500

    initial commit

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="div container">
+            <h4 className="centered">This is the About page</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, distinctio nam dicta mollitia placeat corrupti consequuntur provident dolorem quis voluptatem fuga corporis sapiente eligendi ratione recusandae cum doloribus? Ea, in?</p>
+        </div>
+    )
+}
+export default About
---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="div container">
+            <h4 className="centered">This is the Contact page</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, distinctio nam dicta mollitia placeat corrupti consequuntur provident dolorem quis voluptatem fuga corporis sapiente eligendi ratione recusandae cum doloribus? Ea, in?</p>
+        </div>
+    )
+}
+export default Contact
