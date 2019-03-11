acba1db6172d38026809662f6045466c7e81635a
Andrea <33186021+dec23rd1986@users.noreply.github.com>
2019-03-10 22:42:32 -0400

    initial commit

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Rainbow from '../hoc/Rainbow'
 
 const About = () => {
-    return (
-        <div className="container">
-            <h4 className="center">This is the About page</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, distinctio nam dicta mollitia placeat corrupti consequuntur provident dolorem quis voluptatem fuga corporis sapiente eligendi ratione recusandae cum doloribus? Ea, in?</p>
-        </div>
-    )
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">About</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+      </div>
+    </div>
+  )
 }
-export default Rainbow(About)
+
+export default About
--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
     state ={
@@ -21,7 +22,9 @@ class Home extends Component {
                 return (
                     <div className="post card" key={post.id}>
                         <div className="card-content">
+                            <Link to={'/' + post.id}>
                             <span className="card-title">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -32,7 +35,7 @@ class Home extends Component {
             )
             return (
             <div className="container">
-                <h4 className="center">This is the homepage</h4>
+                <h4 className="center">Home</h4>
                 {postList}
                 </div>
         )
